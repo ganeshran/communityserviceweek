3ce8a8e76ffc9a0c81ad537d83c1260ffb10facb
= <ganesh.ranganathan.85@gmail.com>
2014-01-03 19:17:39 +0530

    Filling show information

--- app/models/sw_interaction.rb
+++ app/models/sw_interaction.rb
@@ -1,4 +1,5 @@
 class SwInteraction < ActiveRecord::Base
 #	attr_accessible :organization, :volunteerHours, :category
-
+	validates :organization, presence: true
+	validates :volunteerHours, presence: true, numericality: true
 end

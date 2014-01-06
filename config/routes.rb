bfcaa55176fe620b1788b10ea2f3f057f5b74a23
= <ganesh.ranganathan.85@gmail.com>
2014-01-06 19:07:55 +0530

    added signin form but login code not yet written

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Crimsonserviceweek::Application.routes.draw do
 	resources :sw_interactions
 	resources :users
+	resources :sessions, only: [:new, :create, :destroy]
 
 	get "home/index"
 	root to: "sw_interactions#index"
 	match '/signup',  to: 'users#new',            via: 'get'
+	match '/signin',  to: 'sessions#new',         via: 'get'
+	match '/signout', to: 'sessions#destroy',     via: 'delete'
 	# The priority is based upon order of creation: first created -> highest priority.
 	# See how all your routes lay out with "rake routes".
 

8fb93fee3e6f94853d87cc1167fe823975b92e0c
= <ganesh.ranganathan.85@gmail.com>
2014-01-06 17:42:14 +0530

    Adding users passwords to users

--- app/models/sw_interaction.rb
+++ app/models/sw_interaction.rb
@@ -2,4 +2,5 @@ class SwInteraction < ActiveRecord::Base
 #	attr_accessible :organization, :volunteerHours, :category
 	validates :organization, presence: true
 	validates :volunteerHours, presence: true, numericality: true
+	validates :activitydate, presence:true
 end

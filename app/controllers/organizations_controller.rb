aac617e784e31e60ed20b11b794e5dce3ec51777
= <ganesh.ranganathan.85@gmail.com>
2014-01-10 19:13:16 +0530

    Styling changes

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,14 +1,21 @@
 class OrganizationsController < ApplicationController
-  def index
-	  @organizations = Organization.all
-  end
+	before_action :signed_in_user, only: [:new, :edit, :show, :index]
+	before_action :admin_user, only: [:edit, :new]
+	def index
+		@organizations = Organization.all
+	end
 
-  def show
-  end
+	def show
+	end
 
-  def edit
-  end
+	def edit
+	end
 
-  def new 
-  end
+	def new 
+	end
+
+	private
+	def admin_user
+		redirect_to(root_url) and flash[:danger] = "You have to be an admin to perform this action" unless current_user.admin?
+	end
 end

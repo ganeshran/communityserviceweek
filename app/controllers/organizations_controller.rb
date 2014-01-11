8bd776bece16d956be668dbc95609eb778e1032e
ganeshran <ganesh.ranganathan.85@gmail.com>
2014-01-11 18:21:50 +0530

    Fixing a bug with organziations

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,7 @@
 class OrganizationsController < ApplicationController
 	before_action :set_organization, only: [:show, :edit, :destroy]
 	before_action :signed_in_user, only: [:new, :edit, :show, :index]
-	before_action :admin_user, only: [:edit, :new]
+	before_action :admin_user, only: [:edit, :new, :destroy]
 	def index
 		@organizations = Organization.all
 	end
@@ -19,7 +19,7 @@ class OrganizationsController < ApplicationController
 	# DELETE /sw_interactions/1
 	# DELETE /sw_interactions/1.json
 	def destroy
-		@other_id = Organization.where(:name => 'Other Institutes').first
+		@other_id = Organization.where(:name => 'Other Institutes').first.id
 		@organization.sw_interactions.update_all(:organization_id => @other_id)
 		@organization.destroy
 		respond_to do |format|
@@ -37,6 +37,6 @@ class OrganizationsController < ApplicationController
 	
 	private
 	def admin_user
-		redirect_to(root_url) and flash[:danger] = "You have to be an admin to perform this action" unless current_user.admin?
+		redirect_to(organizations_url) and flash[:danger] = "You have to be an admin to perform this action" unless current_user.admin?
 	end
 end

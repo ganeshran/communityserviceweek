ff80087079ce33b6c1d2e4a5e9565ad97131f532
= <ganesh.ranganathan.85@gmail.com>
2014-09-22 17:56:27 +0530

    adding a colon to make the error message more clearer

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@advisory\.com\z/
 	validates :email, presence: true, uniqueness: { case_sensitive: false }
-	validates :email, format: { with: VALID_EMAIL_REGEX , message: 'Please use a valid Advisory email to sign up'}
+	validates :email, format: { with: VALID_EMAIL_REGEX , message: ': Please use a valid Advisory email to sign up'}
 
 	before_create :create_remember_token
 
